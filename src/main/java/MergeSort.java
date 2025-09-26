f8528681e9c9666c53031b215667993bc8cc773c
DMR <a1234@DMR-Mac.local>
2025-09-26 23:13:44 +0500

    feat(ci): added GitHub Actions for CI/CD

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void testSum() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+}
